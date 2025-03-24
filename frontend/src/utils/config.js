8e7359fea90668baf6acd2c9cab8c4d7052da6e6
suraj pal <surajpal141516@gmail.com>
2025-03-24 08:40:55 +0530

    port

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://fun-fin-travel.onrender.com" || 'http://localhost:8000'
+export const BASE_URL = "https://fun-fin-travel.onrender.com"
+    // || 'http://localhost:8000'
